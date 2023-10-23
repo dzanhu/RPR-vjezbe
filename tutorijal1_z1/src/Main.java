f697af01167c4783734a5a6d38766e5ecfb8c02c
dzanhu <148075933+dzanhu@users.noreply.github.com>
2023-10-23 15:46:36 +0200

    Add files via upload

---
+++ tutorijal1_z1/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       Scanner scan=new Scanner (System.in);
+
+        System.out.print("Unesite prvi broj: ");
+        int prvibroj=scan.nextInt();
+        System.out.print("Unesite drugi broj: ");
+        int drugibroj=scan.nextInt();
+        System.out.println("Prvi broj je: "+ (prvibroj)+" "+ "Drugi broj je: "+(drugibroj));
+
+
+
+    }
+}
+
