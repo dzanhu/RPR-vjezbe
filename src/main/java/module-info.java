d638543a5c9199a63571a5791b2b3cd4d6f6bc57
dzanhu <dhujic1@etf.unsa.ba>
2023-12-03 19:16:34 +0100

    dd

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.lv6_z1f {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ba.unsa.etf.rpr.tutorijal06 to javafx.fxml;
+    exports ba.unsa.etf.rpr.tutorijal06;
+}
